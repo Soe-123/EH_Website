51f6d7688a8fa01b3e7906719d2ff8c2d1a4cfb6
Soe-123 <82047482+Soe-123@users.noreply.github.com>
2021-04-11 21:28:17 +0100

    Optimising for Mobile

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ $(function() {
     $.scrollify({
       section : ".section-index",
     });
+
   });
+
+  $(window).resize(function() {
+    var width = $(this).width();
+    if(width < 1000) {
+      $.scrollify.disable();
+    } else {
+      $.scrollify.enable();
+    }
+  });
+  
+  $(window).trigger('resize');
+  
+
+ 
+  
